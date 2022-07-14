65a68fba4edf9b69afac0d24c51cc6b14fbfdc92
thuan895 <nguyenvanthuan895@gmail.com>
2022-07-14 11:08:23 +0700

    update

--- Account/serializers.py
+++ Account/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 class SignUpSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True, max_length=255)
     password = serializers.CharField(required=True, max_length=255)
-    userName = serializers.CharField(required=True, max_length=255)
+    username = serializers.CharField(required=True, max_length=255)
     firstName = serializers.CharField(required=True, max_length=255)
     lastName = serializers.CharField(required=True, max_length=255)
 
